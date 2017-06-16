1be50025bebe1170b253743df3b77e647c99be78
mandelangatia <mandelangatia.com>
2017-06-16 15:55:07 +0300

    added the user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,41 @@ Roll.prototype.turn = function() {
     return this.total;
 };
 // User Interface Logic
+$(document).ready(function() {
+  var newroll = new Roll();
+  $("#rollbutton1 button").click(function() {
+    event.preventDefault();
+    var Rollbalance = newroll.turn();
+    $("#score1").html(Rollbalance);
+  });
+  $("#holdbutton1 button").click(function() {
+      var holdnumber = $("#Total1").text() * 1;
+        var displaytotal = holdnumber + newroll.total;
+        $("#Total1").text(displaytotal);
+        $("#score1").text(0);
+        newroll.total = 0;
+        $("#player1").hide();
+        $("#player2").show();
+        if (displaytotal > 100) {
+            alert ("Player 1 has won!!!!!!!!!!")
+        }
+  });
+  $("#rollbutton2 button").click(function() {
+    event.preventDefault();
+    var Rollbalance = newroll.turn();
+    $("#score2").html(Rollbalance);
+  });
+  $("#holdbutton2 button").click(function() {
+      var holdnumber = $("#Total2").text() * 1;
+      var displaytotal = holdnumber + newroll.total;
+      $("#Total2").text(displaytotal);
+        $("#score2").text(0);
+        newroll.total = 0;
+        $("#player2").hide();
+        $("#player1").show();
+        if (displaytotal > 100) {
+            alert ("Player 2 has won!!!!!!!!!!")
+        }
+  });
+
+});
